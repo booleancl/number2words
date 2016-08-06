b62b8898eb9a2c30a8942794bb7a5c589c23854a
Gonzalo Pincheira A <g.pincheira.a@gmail.com>
2016-08-06 10:49:46 -0400

    changes in main function convert and legibility in general for spanish version

--- src/number2words.js
+++ src/number2words.js
@@ -1,178 +1,102 @@
-var numerals,
-    LANGUAGES;
-
-LANGUAGES = {
-  SPANISH: 'es'
-};
-//SPANISH SOURCE
-//SOURCE: http://www.reglasdeortografia.com/numerales.html
-//        http://hispanoteca.eu/Foro-preguntas/ARCHIVO-Foro/Numerales%20uno-una%20-%20formas%20apocopadas.htm
-//        http://smartcubofiles.com/Libros_flip/r3/01secundaria/Leccion02.pdf
-numerals = {
-  
-  '0-29':[
-    'cero',
-    'uno',
-    'dos',
-    'tres',
-    'cuatro',
-    'cinco',
-    'seis',
-    'siete',
-    'ocho',
-    'nueve',
-    'diez',
-    'once',
-    'doce',
-    'trece',
-    'catorce',
-    'quince',
-    'dieciséis',
-    'diecisiete',
-    'dieciocho',
-    'diecinueve',
-    'veinte',
-    'veintiuno',
-    'veintidós',
-    'veintitrés',
-    'veinticuatro',
-    'veinticinco',
-    'veintiséis',
-    'veintisiete',
-    'veintiocho',
-    'veintinueve'
-  ],
-  '30-90':[
-    'treinta',
-    'cuarenta',
-    'cincuenta',
-    'sesenta',
-    'setenta',
-    'ochenta',
-    'noventa'
-  ],
-  
-  '100-900':[
-    'cien',
-    'ciento',
-    'doscientos',
-    'trescientos',
-    'cuatrocientos',
-    'quinientos',
-    'seiscientos',
-    'setecientos',
-    'ochocientos',
-    'novecientos'
-  ],
-  
-  'postfijo': [
-    'mil',
-    'millones',
-    'billones'
-  ],
-
-  '1.000.000': 'millón',
-
-  '1.000.000.000': 'billón',
-
-  //apocopados : http://goo.gl/3NNc0p
-  'apocopados': {
-    '1': ['y un','un'],
-    '21': 'veintiún'
-   }
-};
+var LANGUAGES = {
+      SPANISH: 'es'
+    },
+    dictionary = {
+      // Regla del español.fuente: http://goo.gl/QbJci0
+      //segun la cantidad de ceros sabremos como nombrarlos
+      
+      //utilizar esta herramienta para conocer los nombres de los exponentes muy altos 
+      //http://tip.dis.ulpgc.es/numeros-texto/default.aspx
+      'exponentes': {
+        'singulares': {
+          '6': 'millón',
+          '12': 'billón',
+          '18': 'trillón',
+          '24': 'cuatrillón',
+          '30': 'quintillón',
+          '36': 'sextillón'
+        },
+        'plurales': {
+          '3': 'mil',
+          '6': 'millones',
+          '12': 'billones', 
+          '18': 'trillones',
+          '24': 'cuatrillones',
+          '30': 'quintillones',
+          '36': 'sextillones'
+        }
+      },
+      
+      'apocopados': {
+        '1': 'un',
+        '21': 'veintiún',
+        '100': 'ciento'
+      },
+      
+      //Las decenas están divididas en 2 subconjuntos
+      'decenas': {
+        '0-29': {
+          '0': 'cero','1': 'uno', '2': 'dos', '3': 'tres', '4': 'cuatro', '5': 'cinco',
+          '6': 'seis','7': 'siete', '8': 'ocho', '9': 'nueve', '10': 'diez',
+          '11': 'once', '12': 'doce', '13': 'trece', '14': 'catorce', '15': 'quince',
+          '16': 'dieciseis', '17': 'diecisiete', '18': 'dieciocho', '19': 'diecinueve','20': 'veinte',
+          '21': 'veintiuno', '22':'veintidós', '23': 'veintitres', '24': 'veinticuatro', '25': 'venticinco',
+          '26': 'veintiseis', '27': 'veintisiete', '28': 'veintiocho', '29': 'veintinueve'
+        },
+        '30-90': {
+          '30': 'treinta',
+          '40': 'cuarenta',
+          '50': 'cincuenta',
+          '60': 'sesenta',
+          '70': 'setenta',
+          '80': 'ochenta',
+          '90': 'noventa'
+        }
+      }, //decenas
+      
+      'centenas': {
+        '100': 'cien',
+        '200': 'doscientos',
+        '300': 'trescientos',
+        '400': 'cuatrocientos',
+        '500': 'quinientos',
+        '600': 'seiscientos',
+        '700': 'setecientos',
+        '800': 'ochocientos',
+        '900': 'novecientos'
+      }//centenas
+    };
 
 
+//Pseudo-clase
 module.exports = ConverterToWords;
 
 function ConverterToWords(){
-  var self = this;
-  this.validationRegex = /^(?!0)(?:\d+|\d{1,3}(?:\.\d{3})+)$|^0$/;
+  this.language = 'en';
 }
-ConverterToWords.prototype.setLanguage = function(languageKey){
-  this.language = LANGUAGES[''+languageKey];
+
+ConverterToWords.prototype.setLanguage = function(language){
+  this.language = language;
+}
+
+ConverterToWords.prototype.rules = function(chunk, number_lenght){
+  return dictionary['decenas']['0-29'][chunk];
 };
-ConverterToWords.prototype.isValidFormat = function(number){
-  if(this.validationRegex.test(number)){
-    return true;
-  }
-  throw 'Provide an entire in a valid format';
-};
-ConverterToWords.prototype.getSpecial = function(number){
+
+ConverterToWords.prototype.convert = function(number, chunk_index, total_chunks, converted_text){
+  number += '';
   
-  var specialNumber;
-
-  switch(number){
-    case 1:
-      specialNumber = numerals['apocopados']['1'][0];
-      break;
-    case 21:
-      specialNumber = numerals['apocopados']['21']; //veintiún
-      break;
-  
-  }
-  return specialNumber;
-};
-/* Think about this when you refactoring the function convert
-    http://stackoverflow.com/questions/32347732/efficiency-vs-legibility-of-code
-*/
-ConverterToWords.prototype.convert = function(number, special){
-  
-  var result = '',
-      numberLength,
-      recoursiveCallsCount;
-  
-  if(this.isValidFormat(number)){
-    number = parseInt(('' + number).replace(/\./g,''), 10);
-  }
-
-  if(number >= 0 && number <= 29){
-    result = (number === 21 && special) ? this.getSpecial(21)
-                                        : numerals['0-29'][number];
-  }
-  else if(number >= 30 && number <= 99){
-    result = numerals['30-90'][parseInt(number/10,10) - 3];
-    if(number % 10 !== 0){
-      result += (number%10 === 1 && special) ? ' ' + this.getSpecial(1)
-                                             : ' y ' + this.convert(number%10);
-    }
-  }
-  else if(number >= 100 && number <= 999){
-    if(number === 100){
-      result = numerals['100-900'][0];
-    }
-    else{
-      result = (number/100)%1 === 0 ? numerals['100-900'][parseInt(number/100,10)]
-                                    : numerals['100-900'][parseInt(number/100,10)] + ' ' + ((number%100 === 21 && special) ? this.getSpecial(21) : this.convert(number%100));
-    }
-  }
-  else{
-
-    if(number === 1000){
-      result = numerals['postfijo'][0];
-    }
-    else if(number === 1000000){
-      result = numerals['apocopados']['1'][1] + ' ' + numerals['1.000.000'];
-    }
-    else if(number === 1000000000){
-      result = numerals['apocopados']['1'][1] + ' ' + numerals['1.000.000.000'];
-    }
-    else{
-      splitedNumber = number.toString().match(/(\d{1,3})(?=(\d{3})+(?!\d))/g);
-
-      for(var i = 0; i < splitedNumber.length; i++){
-        if(parseInt(splitedNumber[i], 10) !== 0){
-          result += ' ' + this.convert(parseInt(splitedNumber[i], 10), true) + ' ' + numerals['postfijo'][splitedNumber.length - i - 1];
-        }
-      }
-
-      if(parseInt(number.toString().slice(-3), 10) !== 0){
-        result += ' ' + this.convert(parseInt(number.toString().slice(-3), 10));
-      }
+  var length = total_chunks || Math.ceil(number.length/3),
+      regex = /\d{1,3}(?=(\d{3})*$)/g, //http://goo.gl/oxhsya
+      text = converted_text || '';
       
-    }
-
+  if(!chunk_index) chunk_index = 0;
+  
+  chunk = (number).match(regex)[chunk_index++];
+  text += this.rules(chunk);
+  
+  if(chunk_index === length ){
+    return text;
   }
-  return result.trim();
-
-};
+  return this.convert(number, chunk_index, length, text);
+}
